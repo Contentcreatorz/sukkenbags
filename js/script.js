42e8ced93651f8be747ced515aced6fe2f011e9e
Contentcreatorz <c.oduk@outlook.com>
2023-05-29 16:30:57 +0100

    created the landing page with initial content

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let lastScrollTop = 0; // We'll use this to store the last scroll position
+
+window.addEventListener('scroll', function() {
+  const header = document.getElementById('header');
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) { // If we're scrolling down
+    header.classList.remove('visible');
+  } else { // If we're scrolling up
+    header.classList.add('visible');
+  }
+  
+  lastScrollTop = scrollTop;
+  header.classList.toggle('sticky', window.scrollY > 0);
+});
