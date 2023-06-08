0e01786b02e287aa36a3cf1f0dc2fa0333fda5b7
Contentcreatorz <c.oduk@outlook.com>
2023-06-08 20:44:24 +0100

    stable desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
                 "dark-purple": "#7030A0",
                 "accent-yellow": "#F0E68C",
                 "neutral-white": "#FFFFFF",
-                "neutral-dark": "#4F6EA6"
+                "neutral-dark": "#210436"
       },
     },
   },
