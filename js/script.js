cf5f87de373523a48115c829fa7f7d410151991c
Contentcreatorz <c.oduk@outlook.com>
2023-06-09 19:44:05 +0100

    fixes footer to have columns on smaller screens

--- js/script.js
+++ js/script.js
@@ -1,68 +1,71 @@
-AOS.init()
+AOS.init();
 
-let lastScrollTop = 0
-let header = document.getElementById('header')
+let lastScrollTop = 0;
+let header = document.getElementById('header');
 
 function handleHeaderScroll() {
-	const header = document.getElementById('header')
-	let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+  const header = document.getElementById('header');
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-	if (scrollTop > lastScrollTop) {
-		header.style.transform = 'translateY(-100%)'
-	} else {
-		header.style.transform = 'translateY(0)'
-	}
+  if (scrollTop > lastScrollTop) {
+    header.style.transform = 'translateY(-100%)';
+  } else {
+    header.style.transform = 'translateY(0)';
+  }
 
-	if (window.scrollY <= 5) {
-		header.classList.add('transparent')
-	} else {
-		header.classList.remove('transparent')
-		header.style.position = 'fixed'
-	}
+  if (window.scrollY <= 5) {
+    header.classList.add('transparent');
+  } else {
+    header.classList.remove('transparent');
+    header.style.position = 'fixed';
+  }
 
-	lastScrollTop = scrollTop
+  lastScrollTop = scrollTop;
 }
 
-window.addEventListener('scroll', handleHeaderScroll)
+window.addEventListener('scroll', handleHeaderScroll);
 
-window.addEventListener('resize', AOS.refresh)
+window.addEventListener('resize', AOS.refresh);
 
-document.addEventListener('DOMContentLoaded', function () {
-	let isSidebarOpen = false
+document.addEventListener("DOMContentLoaded", function () {
+  let isSidebarOpen = false;
 
-	document.getElementById('mobile-menu-button').addEventListener('click', function () {
-		var mobileNav = document.getElementById('mobile-nav')
-		var mySidebar = document.getElementById('mySidebar')
-		var header = document.getElementById('header')
-		Array.from(document.getElementsByClassName('nav-links')).forEach(function (link) {
-			link.addEventListener('click', function () {
-				mobileNav.style.transform = 'translateX(100%)'
-				mySidebar.classList.remove('show-sidebar')
-				header.style.transform = 'translateY(0%)'
-				isSidebarOpen = false
-			})
-		})
+  document.getElementById("mobile-menu-button").addEventListener("click", function () {
+    var mobileNav = document.getElementById("mobile-nav");
+    var mySidebar = document.getElementById("mySidebar");
+    var header = document.getElementById("header");
+    Array.from(document.getElementsByClassName("nav-links")).forEach(function (link) {
+      link.addEventListener("click", function () {
+        mobileNav.style.transform = 'translateX(100%)';
+          mySidebar.classList.remove("show-sidebar");
+          window.addEventListener('scroll', handleHeaderScroll);
+          isSidebarOpen = false;
+      });
+    });
 
-		if (!isSidebarOpen) {
-			mobileNav.style.transform = 'translateX(0%)'
-			header.style.transform = 'translateY(-100%)'
-			mySidebar.classList.add('show-sidebar')
-			window.removeEventListener('scroll', handleHeaderScroll)
-			isSidebarOpen = true
-		}
-	})
 
-	document.getElementById('close-mobile-nav').addEventListener('click', function () {
-		var mobileNav = document.getElementById('mobile-nav')
-		var mySidebar = document.getElementById('mySidebar')
-		var header = document.getElementById('header')
+    if (!isSidebarOpen) {
+      mobileNav.style.transform = 'translateX(0%)';
+      header.style.transform = 'translateY(-100%)';
+      mySidebar.classList.add("show-sidebar");
+      window.removeEventListener('scroll', handleHeaderScroll);
+      isSidebarOpen = true;
+    }
+  });
 
-		if (isSidebarOpen) {
-			mobileNav.style.transform = 'translateX(100%)'
-			header.style.transform = 'translateY(0)'
-			mySidebar.classList.remove('show-sidebar')
-			window.addEventListener('scroll', handleHeaderScroll)
-			isSidebarOpen = false
-		}
-	})
-})
+  document.getElementById("close-mobile-nav").addEventListener("click", function () {
+    var mobileNav = document.getElementById("mobile-nav");
+    var mySidebar = document.getElementById("mySidebar");
+    var header = document.getElementById("header");
+
+    if (isSidebarOpen) {
+      mobileNav.style.transform = 'translateX(100%)';
+      setTimeout(function () {
+        header.style.transform = 'translateY(0)';
+        mySidebar.classList.remove("show-sidebar");
+        window.addEventListener('scroll', handleHeaderScroll);
+        isSidebarOpen = false;
+      }, 500);
+    }
+  });
+});
