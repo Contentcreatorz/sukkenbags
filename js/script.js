2c968812eb392a43da63d08eca209407e5c4291d
Contentcreatorz <c.oduk@outlook.com>
2023-06-12 14:04:37 +0100

    formated source code to remove redundacy

--- js/script.js
+++ js/script.js
@@ -1,92 +1,91 @@
-AOS.init();
+AOS.init()
 
-let lastScrollTop = 0;
-let header = document.getElementById('header');
+let lastScrollTop = 0
+let header = document.getElementById('header')
 
 function handleHeaderScroll() {
-  const header = document.getElementById('header');
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	const header = document.getElementById('header')
+	let scrollTop = window.pageYOffset || document.documentElement.scrollTop
 
-  if (scrollTop > lastScrollTop) {
-    header.style.transform = 'translateY(-100%)';
-  } else {
-    header.style.transform = 'translateY(0)';
-  }
+	if (scrollTop > lastScrollTop) {
+		header.style.transform = 'translateY(-100%)'
+	} else {
+		header.style.transform = 'translateY(0)'
+	}
 
-  if (window.scrollY <= 5) {
-    header.classList.add('transparent');
-  } else {
-    header.classList.remove('transparent');
-    header.style.position = 'fixed';
-  }
+	if (window.scrollY <= 5) {
+		header.classList.add('transparent')
+	} else {
+		header.classList.remove('transparent')
+		header.style.position = 'fixed'
+	}
 
-  lastScrollTop = scrollTop;
+	lastScrollTop = scrollTop
 }
 
-window.addEventListener('scroll', handleHeaderScroll);
+window.addEventListener('scroll', handleHeaderScroll)
 
-window.addEventListener('resize', AOS.refresh);
+window.addEventListener('resize', AOS.refresh)
 
-document.addEventListener("DOMContentLoaded", function () {
-  let isSidebarOpen = false;
+document.addEventListener('DOMContentLoaded', function () {
+	let isSidebarOpen = false
 
-  document.getElementById("mobile-menu-button").addEventListener("click", function () {
-    var mobileNav = document.getElementById("mobile-nav");
-    var mySidebar = document.getElementById("mySidebar");
-    var header = document.getElementById("header");
-    Array.from(document.getElementsByClassName("nav-links")).forEach(function (link) {
-      link.addEventListener("click", function () {
-        mobileNav.style.transform = 'translateX(100%)';
-          mySidebar.classList.remove("show-sidebar");
-          window.addEventListener('scroll', handleHeaderScroll);
-          isSidebarOpen = false;
-      });
-    });
+	document.getElementById('mobile-menu-button').addEventListener('click', function () {
+		var mobileNav = document.getElementById('mobile-nav')
+		var mySidebar = document.getElementById('mySidebar')
+		var header = document.getElementById('header')
+		Array.from(document.getElementsByClassName('nav-links')).forEach(function (link) {
+			link.addEventListener('click', function () {
+				mobileNav.style.transform = 'translateX(100%)'
+				mySidebar.classList.remove('show-sidebar')
+				window.addEventListener('scroll', handleHeaderScroll)
+				isSidebarOpen = false
+			})
+		})
 
+		if (!isSidebarOpen) {
+			mobileNav.style.transform = 'translateX(0%)'
+			header.style.transform = 'translateY(-100%)'
+			mySidebar.classList.add('show-sidebar')
+			window.removeEventListener('scroll', handleHeaderScroll)
+			isSidebarOpen = true
+		}
+	})
 
-    if (!isSidebarOpen) {
-      mobileNav.style.transform = 'translateX(0%)';
-      header.style.transform = 'translateY(-100%)';
-      mySidebar.classList.add("show-sidebar");
-      window.removeEventListener('scroll', handleHeaderScroll);
-      isSidebarOpen = true;
-    }
-  });
+	document.getElementById('close-mobile-nav').addEventListener('click', function () {
+		var mobileNav = document.getElementById('mobile-nav')
+		var mySidebar = document.getElementById('mySidebar')
+		var header = document.getElementById('header')
 
-  document.getElementById("close-mobile-nav").addEventListener("click", function () {
-    var mobileNav = document.getElementById("mobile-nav");
-    var mySidebar = document.getElementById("mySidebar");
-    var header = document.getElementById("header");
+		if (isSidebarOpen) {
+			mobileNav.style.transform = 'translateX(100%)'
+			setTimeout(function () {
+				header.style.transform = 'translateY(0)'
+				mySidebar.classList.remove('show-sidebar')
+				window.addEventListener('scroll', handleHeaderScroll)
+				isSidebarOpen = false
+			}, 500)
+		}
+	})
+})
 
-    if (isSidebarOpen) {
-      mobileNav.style.transform = 'translateX(100%)';
-      setTimeout(function () {
-        header.style.transform = 'translateY(0)';
-        mySidebar.classList.remove("show-sidebar");
-        window.addEventListener('scroll', handleHeaderScroll);
-        isSidebarOpen = false;
-      }, 500);
-    }
-  });
-});
+window.addEventListener('DOMContentLoaded', event => {
+	fetch('../data/testimonials.json')
+		.then(response => response.json())
+		.then(data => {
+			const testimonialsContainer = document.querySelector('#testimonials .flex')
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    fetch('../data/testimonials.json')
-        .then(response => response.json())
-        .then(data => {
-            const testimonialsContainer = document.querySelector('#testimonials .flex');
+			testimonialsContainer.innerHTML = ''
 
-            testimonialsContainer.innerHTML = '';
-
-          data.testimonials.forEach(testimonial => {
-                testimonialsContainer.innerHTML += `
+			data.testimonials.forEach(testimonial => {
+				testimonialsContainer.innerHTML += `
                     <div data-aos="zoom-in-up" class="w-full sm:w-1/2 p-4">
                         <blockquote class="border-l-4 border-purple-700 pl-4 italic mb-4">
                             <p>"${testimonial.quote}"</p>
                         </blockquote>
                         <cite class="font-bold text-dark-purple">- ${testimonial.author}</cite>
                     </div>
-                `;
-            });
-        });
-});
+                `
+			})
+		})
+})
